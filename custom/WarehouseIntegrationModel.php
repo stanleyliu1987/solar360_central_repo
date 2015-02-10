3cd9e63b1c6226518aa959f1f2f1ad05b5270c7e
stanleyliu1006 <stanleyliu1006@oultook.com>
2015-02-10 18:05:48 +1100

    Warehouse integration bug fix

--- custom/WarehouseIntegrationModel.php
+++ custom/WarehouseIntegrationModel.php
@@ -28,16 +28,16 @@ while ($item = DB_fetch_array($result)) {
     $export_data.=$item['PH'].str_repeat("^", 3).$item['Customer #'].str_repeat("^", 1).$item['Order Number'].str_repeat("^", 7).
                   $item["Ship Name"].str_repeat("^", 1).$item["Ship Address 1"].str_repeat("^", 2).$item["Ship City"].str_repeat("^", 1).
                   $item["Ship State"].str_repeat("^", 1).$item["Ship Post Code"].str_repeat("^", 1).$item["Ship Country"].str_repeat("^", 2).$item["Ship Phone #"].
-                  str_repeat("^", 8).date("Ymd", strtotime($item["Ship Date Required"])).str_repeat("^", 7).date("Ymd").str_repeat("^", 6)."SLR".str_repeat("^", 1).$item["Special Instructions 1"].str_repeat("^", 38).
-                  $item["Business Telephone Number"].str_repeat("^", 1).$item["Email Address"].str_repeat("^", 30)."\r\n";
+                  str_repeat("^", 8).date("Ymd", strtotime($item["Ship Date Required"])).str_repeat("^", 7).date("Ymd").str_repeat("^", 6)."SLR".str_repeat("^", 1).preg_replace('/\s+/', ' ', trim( $item["Special Instructions 1"])).str_repeat("^", 38).
+                  $item["Business Telephone Number"].str_repeat("^", 1).$item["Email Address"].str_repeat("^", 30)."\n";
     
     $sql_pd = "SELECT * FROM pd_kingswarehouse where `ORDER Number`='".$item['Order Number']."'";
     $ErrMsg =  _('An error occurred selecting all po details');
     $DbgMsg =  _('The SQL that was used to select po details and failed in the process was'); 
     $result_PD = DB_query($sql_pd,$this->db,$ErrMsg,$DbgMsg);
     while ($item_PD = DB_fetch_array($result_PD)) { 
-    $export_data.=$item_PD['PD'].str_repeat("^", 1)."R".str_repeat("^", 1)."ROW".str_repeat("^", 2).$item_PD['Order Number'].str_repeat("^", 7).$item_PD['Product Code'].str_repeat("^", 2).$this->stripcomma($item_PD['Description']).str_repeat("^", 4).
-                  $item_PD['Quantity Ordered'].str_repeat("^", 1).$item_PD['Quantity Pick'].str_repeat("^", 21).str_repeat("^", 44)."\r\n";   
+    $export_data.=$item_PD['PD'].str_repeat("^", 1)."R".str_repeat("^", 1)."ROW".str_repeat("^", 2).$item_PD['Order Number'].str_repeat("^", 7).$item_PD['Product Code'].str_repeat("^", 2).preg_replace('/\s+/', ' ', trim($this->stripcomma($item_PD['Description']))).str_repeat("^", 4).
+                  $item_PD['Quantity Ordered'].str_repeat("^", 1).$item_PD['Quantity Pick'].str_repeat("^", 21).str_repeat("^", 44)."\n";   
     }
 }
 /* Write the file to a folder */
